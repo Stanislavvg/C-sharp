d9cfd461b68a3db1f427f57dedf94e2166e2185b
Alexander <slava.grischenko@yandex.ru>
2023-06-22 21:41:27 +0300

    refactor: add the description to Задача 2

---
+++ Seminar 1/Project 1/Задача 2.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.*/
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1<num2)
+{
+    Console.WriteLine($"{num1} меньше, чем {num2}.");
+}
+
+if (num1>num2)
+{
+    Console.WriteLine($"{num1} больше, чем {num2}.");
+}
+
+if (num1 == num2)
+{
+    Console.WriteLine("Вы ввели два одинаковых числа.");
+}
