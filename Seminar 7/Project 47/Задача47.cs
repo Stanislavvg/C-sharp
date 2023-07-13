d5cd59a188a6a4f9241875939b027e2859f35319
Alexander <slava.grischenko@yandex.ru>
2023-07-13 17:14:21 +0300

    feat: finish Задача 47 project

---
+++ Seminar 7/Project 47/Задача47.cs
@@ -0,0 +1,28 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов:");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+var random = new Random();
+
+for (int x = 0; x < array.GetLength(0); x++)
+{
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+        array[x, y] = Math.Round(random.NextDouble(), 1);
+    }
+}
+
+for (int x = 0; x < array.GetLength(0); x++)
+{
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+        Console.Write(array[x, y] + "\t");
+    }
+    Console.WriteLine();
+}
