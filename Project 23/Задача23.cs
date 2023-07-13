2305a2ba436e8a1026039f51b7f2632065da2f06
Stanislavvg <136792256+Stanislavvg@users.noreply.github.com>
2023-07-13 22:08:11 +0300

    Delete Project 23 directory

--- Project 23/Задача23.cs
+++
@@ -1,9 +0,0 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
-Console.WriteLine("Введите число N: ");
-int number = int.Parse(Console.ReadLine());
-
-for (int count = 1; count <= number; count++)
-{
-    Console.WriteLine($"Число {count} в третьей степени равно {count * count * count}");
-}
