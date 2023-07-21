97f79f3cf82179fd03342a0d7ce7e92857f5f2ab
Alexander <slava.grischenko@yandex.ru>
2023-07-22 01:31:29 +0300

    feat: finish Задача 66 project

---
+++ Seminar 9/Project 66/Задача66.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (m < n)
+{
+    int SummMethod(int start)
+    {
+        if (start == n) return start;
+        if (start < n) return start + SummMethod(start + 1);
+        else return 0;
+    }
+    Console.WriteLine(SummMethod(m));
+}
+else
+{
+    int SummMethod(int start)
+    {
+        if (start == m) return start;
+        if (start < m) return start + SummMethod(start + 1);
+        else return 0;
+    }
+    Console.WriteLine(SummMethod(n));
+}
+
