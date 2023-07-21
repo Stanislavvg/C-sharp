53e4f719995dab9c6ce1a609f5a5568a774adef8
Alexander <slava.grischenko@yandex.ru>
2023-07-22 01:29:05 +0300

    feat: finish Задача 64 project

---
+++ Seminar 9/Project 64/Задача64.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+string PrintNumbers(int number)
+{
+    if (number == 1) return 1.ToString();
+    if (number > 1) return number.ToString() + ", " + PrintNumbers(number - 1);
+    else
+    {
+        return " ";
+    }
+
+}
+
+Console.WriteLine(PrintNumbers(N));
